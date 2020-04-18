446c7314d633278a02efebf81df65dfe67c8d4f5
Charles Gery <charles.gery@gmail.com>
2020-04-18 14:11:20 +0200

    Modify App.js
    Moved the logic of the application to App.js : does the main computation
    and transfer the results to child components.
    Add a new Calendar component in children.
    Data is now fetched when the calendar finish getting current week. This
    will allow in the future to directly select week in calendar.
    App.js also handles the clicks on a day in calendar and transfer the
    selected day to the list of offers.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import moment from 'moment';
 
 import ListOffers from './components/ListOffers';
+import Calendar from './components/Calendar';
 
 import data from './components/data.json'
 
@@ -17,45 +18,112 @@ class App extends React.Component {
       offers: [],
       links: {},
       nextPage: null,
+      startOfWeek: null,
+      endOfWeek: null,
     };
     */
+
     this.state = {
-      offers: data.results,
+      offers: [],
       links: data.links,
       nextPage: data.nextPage,
+      startOfWeek: null,
+      endOfWeek: null,
+      selectedDay: null,
     }
+    
+    this.handleWeekChange = this.handleWeekChange.bind(this);
+    this.handleSelectDay = this.handleSelectDay.bind(this);
+    this.getOffersPerDay = this.getOffersPerDay.bind(this);
+    this.getDaysOfWeek = this.getDaysOfWeek.bind(this);
+
   }
 
-  /*
-  componentDidMount() {
+  handleWeekChange(startOfWeek, endOfWeek) {
 
+    this.setState({
+      startOfWeek: startOfWeek,
+      endOfWeek: endOfWeek,
+    });
 
-    var startOfWeek = moment().weekday(0).format("Y-MM-DD");
-    var endOfWeek = moment().weekday(6).format("Y-MM-DD");
     var companyId = 6;
     var pageNumber = 1;
     var pageSize = 10;
+    var formattedStartOfWeek = startOfWeek.format("Y-MM-DD");
+    var formattedEndOfWeek = endOfWeek.format("Y-MM-DD");
 
-    fetch(`https://back.staging.bsport.io/api/v1/offer/?min_date=${startOfWeek}&max_date=${endOfWeek}&company=${companyId}&page=${pageNumber}&page_size=${pageSize}`)
-      .then(data => data.json())
-      .then(data => {
-        this.setState({
-          offers: data.results,
-          nextPage: data.nextPage,
-          links: data.links
-        })
+    fetch(`https://back.staging.bsport.io/api/v1/offer/?min_date=${formattedStartOfWeek}&max_date=${formattedEndOfWeek}&company=${companyId}&page=${pageNumber}&page_size=${pageSize}`)
+    .then(data => data.json())
+    .then(data => {
+      this.setState({
+        offers: data.results,
+        nextPage: data.nextPage,
+        links: data.links
       });
-     
+    });
+
   }
-  */
+
+  handleSelectDay(date) {
+    this.setState({
+      selectedDay: date,
+    });
+  }
+
+  getOffersPerDay(offers) {
+
+    var offersPerDay = [];
+    var weekDays = this.getDaysOfWeek();
+
+    if(weekDays.length > 0){
+      for(let i=0; i<weekDays.length; i++) {
+        offersPerDay.push({
+          date: weekDays[i],
+          offers: [],
+        });
+      }
+  
+      for(let i=0; i<offers.length; i++) {
+        
+        var offerDate = moment(offers[i].date_start);
+        var dayOfWeek = offerDate.weekday();
+  
+        offersPerDay[dayOfWeek]['offers'].push(offers[i]);
+      }
+    }
+
+    return offersPerDay;
+
+  }
+
+  getDaysOfWeek() {
+    
+    var weekDays = [];
+
+    if (this.state.startOfWeek){
+      var dayOfWeek = moment(this.state.startOfWeek);
+
+      weekDays.push(moment(dayOfWeek));
+      for(let i=0; i<6; i++){
+        weekDays.push(moment(dayOfWeek.add(1, 'd')));
+      }
+    }
+
+    return weekDays;
+  }
+  
 
   render() {
 
     const offers = this.state.offers;
+    const startOfWeek = this.state.startOfWeek;
+    const selectedDay = this.state.selectedDay;
+    const offersPerDay = this.getOffersPerDay(offers);
 
     return (
     <div className="App">
-      <ListOffers offers={offers}/> 
+      <Calendar onWeekChange={this.handleWeekChange} onSelectDayChange={this.handleSelectDay} offersPerDay={offersPerDay}/>
+      <ListOffers offersPerDay={offersPerDay} startOfWeek={startOfWeek} selectedDay={selectedDay} />
     </div>
     );
 
