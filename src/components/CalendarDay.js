3e2bdaf4967d3c1689505afc3b4569c0263c8703
Charles Gery <charles.gery@gmail.com>
2020-04-19 11:21:19 +0200

    Modify CalendarDay.js
    Add documentation of component.
    Add a case for when the number of offers is too big to display.

--- src/components/CalendarDay.js
+++ src/components/CalendarDay.js
@@ -2,6 +2,11 @@ import React from 'react';
 
 import '../App.css';
 
+/**
+ * Displays a calendar day
+ * with the name of the day and the number 
+ * of offers this day
+ */
 class CalendarDay extends React.Component {
 
   render() {
@@ -9,13 +14,20 @@ class CalendarDay extends React.Component {
     const date = this.props.date;
     const numOffers = this.props.numOffers;
 
+    let stringNumOffers;
+    if (numOffers < 10) {
+      stringNumOffers = '*'.repeat(numOffers);
+    } else {
+      stringNumOffers = '*'.repeat(10).concat('+');
+    }
+
     return (
     <div className="calendarDay">
         <div>
             {date.format("ddd, DD")}
         </div>
         <div>
-            {'*'.repeat(numOffers)}
+            {stringNumOffers}
         </div>
     </div>
     );
